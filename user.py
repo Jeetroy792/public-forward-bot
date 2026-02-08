641f73e86642a5e834f1cf9065abce6dc3c8eeee
Jeetroy792 <jeetroy5335@gmail.com>
2026-02-08 19:26:50 +0530

    Refactor message sending and user retrieval logic

--- user.py
+++ user.py
@@ -2,7 +2,6 @@ from config import Config
 from config import LOGGER
 from pyrogram import Client
 from pyrogram.session import StringSession
-import asyncio
 
 BOT_USERNAME = Config.BOT_USERNAME
 
@@ -20,9 +19,9 @@ class User(Client):
     async def start(self):
         await super().start()
         if BOT_USERNAME:
-            await User.send_message(self, chat_id=BOT_USERNAME, text="/forward")
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
+            await self.send_message(chat_id=BOT_USERNAME, text="/forward")
+        me = await self.get_me()
+        return self, me.id
 
     async def stop(self, *args):
         await super().stop()
